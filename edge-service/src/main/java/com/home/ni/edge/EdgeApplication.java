cbc294022e0677b5062d67429968ffcf2d3d4e6f
nathan <myopportunity@163.com>
2017-06-19 23:49:49 +0800

    add oauth2, but some problem now

--- edge-service/src/main/java/com/home/ni/edge/EdgeApplication.java
+++ edge-service/src/main/java/com/home/ni/edge/EdgeApplication.java
@@ -2,13 +2,26 @@ package com.home.ni.edge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateCustomizer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.ribbon.RibbonClientHttpRequestFactory;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.token.AccessTokenProviderChain;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordAccessTokenProvider;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by Administrator on 2017/6/17.
@@ -31,5 +44,21 @@ public class EdgeApplication {
         protected void configure(HttpSecurity http) throws Exception {
             http.csrf().disable();
         }
+    }
+
+
+    @Bean
+    UserInfoRestTemplateCustomizer userInfoRestTemplateCustomizer(SpringClientFactory springClientFactory) {
+        return template -> {
+            AccessTokenProviderChain accessTokenProviderChain = Stream
+                    .of(
+                            new AuthorizationCodeAccessTokenProvider(),
+                            new ImplicitAccessTokenProvider(),
+                            new ResourceOwnerPasswordAccessTokenProvider(),
+                            new ClientCredentialsAccessTokenProvider())
+                    .peek(tp -> tp.setRequestFactory(new RibbonClientHttpRequestFactory(springClientFactory)))
+                    .collect(Collectors.collectingAndThen(Collectors.toList(), AccessTokenProviderChain::new));
+            template.setAccessTokenProvider(accessTokenProviderChain);
+        };
     }
 }
--- user-service/src/main/java/com/home/ni/user/UserApplication.java
+++ user-service/src/main/java/com/home/ni/user/UserApplication.java
@@ -5,7 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import java.security.Principal;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * Created by Administrator on 2017/6/17.
  */
@@ -14,9 +21,30 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableDiscoveryClient
 @EnableZuulProxy
 @EnableHystrix
+@EnableResourceServer
 public class UserApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+    }
+
+
+
+    @Controller
+    @RequestMapping("/")
+    public static class DummyController {
+
+        @RequestMapping(method = RequestMethod.GET)
+        @ResponseBody
+        public String helloWorld(Principal principal) {
+            return principal == null ? "Hello anonymous" : "Hello " + principal.getName();
+        }
+
+        @PreAuthorize("#oauth2.hasScope('openid') and hasRole('ROLE_ADMIN')")
+        @RequestMapping(value = "secret", method = RequestMethod.GET)
+        @ResponseBody
+        public String helloSecret(Principal principal) {
+            return principal == null ? "Hello anonymous" : "S3CR3T  - Hello " + principal.getName();
+        }
     }
 }
